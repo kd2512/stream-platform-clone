4588bb8d34fa69fbeeebdf6425ede7ec0effda1a
kdivyans <kdivyans@cisco.com>
2023-08-18 00:49:36 +0530

    added firebase for hosting

---
+++ firebase.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDzlMOGpHKjtMkDYqu7yOtnPRMwXvPZZl8",
+	authDomain: "cyber-flix.firebaseapp.com",
+	projectId: "cyber-flix",
+	storageBucket: "cyber-flix.appspot.com",
+	messagingSenderId: "704459617593",
+	appId: "1:704459617593:web:a77bf2834e8ace651aa4e8",
+	measurementId: "G-KKVWM62EZX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
